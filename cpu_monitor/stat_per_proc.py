1860fe521c4312ff15cb8151d1181804b5bcedb4
Thomas <tkostas75@gmail.com>
2017-10-20 19:05:49 +0900

    added getting data from /proc/[pid]/stat listing it still have some bugs

--- cpu_monitor/stat_per_proc.py
+++ cpu_monitor/stat_per_proc.py
@@ -1,5 +1,8 @@
 import glob
 import time
+import os
+import re
+
 def get_proc_list():
     proc_list = glob.glob("/proc/[0-9]*")
     proc_stat_list = []
@@ -32,7 +35,60 @@ def generate_proc_stat_fp(previous_proc_list, actual_proc_list, proc_stat_fp_dic
     return actual_proc_list
 
 
+def get_cpu_tick_per_second():
+    return os.sysconf('SC_CLK_TCK')
 
+
+def get_provided_values_list():
+    # http://man7.org/linux/man-pages/man5/proc.5.html
+    values = ['pid', 'comm', 'state', 'ppid', 'pgrp', 'session', 'tty_nr', 'tpgid', 'flags', 'minflt', 'cminflt',
+              'majflt', 'cmajflt', 'utime', 'stime', 'cutime', 'cstime', 'priority', 'nice', 'num_threads',
+              'itrealvalue', 'starttime', 'vsize', 'rss', 'rsslim', 'startcode', 'endocode', 'startstack', 'kstkesp',
+              'kstkeip', 'signal', 'blocked', 'sigignore', 'sigcatch', 'wchan', 'nswap', 'cnswap', 'exit_signal',
+              'processor', 'rt_priority', 'policy', 'delayacct_blkio_ticks', 'guest_time', 'cguest_time', 'start_data',
+              'end_data', 'start_brk', 'arg_start', 'arg_end', 'env_start', 'env_end', 'exit_code']
+    return values
+
+
+def print_proc_stat_dict(proc_stat_dict):
+    for element in get_provided_values_list():
+        value = proc_stat_dict.get(element)
+        if value:
+            print element + ":" + value
+
+
+def parse_process_stat(stat_file_fp):
+    if (stat_file_fp):
+        data_dict= {}
+        data_names = get_provided_values_list()
+        data = stat_file_fp.read()
+        stat_file_fp.seek(0)
+        print data
+        name = re.findall(" \(.*\) ", data)
+        try:#ask forgiveness not permission
+            name = name[0]
+        except IndexError:
+            name = "no_name"
+            print "no name_found ??"
+        ##name = name.group(1)
+
+
+        # PLZ IMPROVE ME
+        data_list = re.split('\(.*\)',data)
+        data_list = re.split(' *',data_list[0])  + re.split(' *',data_list[1])
+        data_list.insert(1, name)
+
+        data_names_counter = 0
+        for data in data_list:
+
+            print len(data_names)
+            print data_names_counter
+
+            data_name = data_names[data_names_counter]
+            data_dict[data_name] = data
+            data_names_counter += 1
+        print data_dict
+        return data_dict
 
 
 
@@ -42,6 +98,10 @@ def main():
     while True:
         proc_list = get_proc_list()
         previous_proc_list = generate_proc_stat_fp(previous_proc_list, proc_list, proc_fp_dict)
-        time.sleep(0.01)
+
+        for key,file_pointer in proc_fp_dict.iteritems():
+            data_dict = parse_process_stat(file_pointer)
+            print_proc_stat_dict(data_dict)
+        time.sleep(1)
 
 main()
