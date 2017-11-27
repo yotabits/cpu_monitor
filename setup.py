88ab9ae3720a25f8e44ef27db69ad8d4f23b143b
Thomas <tkostas75@gmail.com>
2017-11-27 15:23:57 +0900

    Added classifiers informations in steup.py for release

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(name='cpu_monitor',
+      packages=['cpu_monitor'],
       version='0.1',
       description='Cpu monitoring and burning tool',
       author='yotabits',
       author_email='tkostas75@gmail.com',
       license='MIT',
       url='https://github.com/yotabits/cpu_monitor',
-      packages=['cpu_monitor'],
+      download_url='https://github.com/yotabits/cpu_monitori/archive/0.1.tar.gz',
       scripts=['bin/cpu_monitor'],
+      keywords='monitoring cpu',
       install_requires=[
           'py-cpuinfo',
           'plotly',
@@ -18,4 +20,11 @@ setup(name='cpu_monitor',
           'dash_core_components',
           'dash_html_components'
       ],
+        classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ],
       zip_safe=False)
